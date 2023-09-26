16cfb7a9a38df8b9ec64cf40bf230a1ec9e181a9
Eduardo Aguilar <eaguilar92105@yahoo.com>
2023-09-26 01:11:03 -0700

    added navigation packages to start routing

--- App.jsx
+++ App.jsx
@@ -1,18 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './components/home';
 import UtilityBar from './components/utilityBar';
 import Banner from './components/banner';
 import Grid from './components/grid';
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.body}>
-      <UtilityBar />
-      <Banner />
-      <Home />
-      <Grid />
-    </View>
+        <View style={styles.body}>
+          <UtilityBar />
+          <Banner />
+          <Home />
+          <Grid />
+        </View>
   );
 }
 

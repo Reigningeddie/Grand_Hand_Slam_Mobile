967a5eb5eae36d6fb406b7e142d40182da55baf8
Eduardo Aguilar <eaguilar92105@yahoo.com>
2023-09-26 16:30:33 -0700

    working with header styles

--- App.jsx
+++ App.jsx
@@ -7,22 +7,52 @@ import UtilityBar from './components/utilityBar';
 import Banner from './components/banner';
 import Grid from './components/grid';
 
+
+function HomeScreen() {
+  return (
+    <View style={styles.body}>
+      {/* <UtilityBar /> */}
+      <Banner />
+      <Home />
+      <Grid />
+    </View>
+  )
+};
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-        <View style={styles.body}>
-          <UtilityBar />
-          <Banner />
-          <Home />
-          <Grid />
-        </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+        name="Home" 
+        component={HomeScreen}
+        options={{
+          title: 'Grand Hand Slam',
+          headerStyle: {
+    backgroundColor: '#2D4356',
+    borderWidth: 0,
+    elevation: 0,
+    shadowOpacity: 0
+          },
+          headerTintColor: '#EAB2A0',
+          headerTitleStyle: {
+            fontSize: 40,
+          }
+        }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
   body: {
     flex: 1,
     backgroundColor: '#435b66',
   },
+
+  header: {
+    backgroundColor: '#2D4356',
+  }
 });

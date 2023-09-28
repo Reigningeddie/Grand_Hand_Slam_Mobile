09ebb7acc923f1d54080fe3ca6ebe754ac67a906
Eduardo Aguilar <eaguilar92105@yahoo.com>
2023-09-27 17:41:27 -0700

    created a screens folder to de clutter app.js

--- App.jsx
+++ App.jsx
@@ -1,23 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, SafeAreaProvider } from 'react-native';
+import { Text, View, SafeAreaView, SafeAreaProvider } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './components/home';
-import UtilityBar from './components/utilityBar';
-import Banner from './components/banner';
-import Grid from './components/grid';
+import HomeScreen from './screens/homeScreen'
 
 
-function HomeScreen() {
-  return (
-    <View style={styles.body}>
-      <UtilityBar />
-      <Banner />
-      <Home />
-      <Grid />
-    </View>
-  )
-};
 
 const Stack = createNativeStackNavigator();
 
@@ -28,19 +15,10 @@ export default function App() {
         <Stack.Screen 
         name="Home" 
         component={ HomeScreen }
-        options={{ headerShown: false }} />
+        options={{ headerShown: false}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
-const styles = StyleSheet.create({
-  body: {
-    flex: 1,
-    backgroundColor: '#435b66',
-  },
 
-  header: {
-    backgroundColor: '#2D4356',
-  }
-});

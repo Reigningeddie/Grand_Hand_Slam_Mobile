9742812f1de398717829d6d421eb1ec7544bd8c0
Eduardo Aguilar <eaguilar92105@yahoo.com>
2023-10-02 01:46:06 -0700

    working on leaderboard screen

--- app/leaderBoard.jsx
+++ app/leaderBoard.jsx
@@ -2,6 +2,18 @@ import { View, Text, StyleSheet } from 'react-native';
 
 export default function LeaderBoard() {
   return (
-    <Text>Hello</Text>
+    <View style={styles.body}>
+      <Text>Hello I am the leaderBoard</Text>
+    </View>
+
   )
 }
+
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    backgroundColor: '#435B66',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
